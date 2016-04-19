75ef23b98d229898e32d424e500c5eda2a564bcf
Jean-Philippe DIDIER <jphdidier@gmail.com>
2016-04-19 15:13:41 +0200

    toujours des espaces

--- src/main/java/org/javaee7/sample/Person.java
+++ src/main/java/org/javaee7/sample/Person.java
@@ -1,31 +1,31 @@
-package org.javaee7.sample;
+package org.javaee7.sample ;
 
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlRootElement ;
 
 /**
  * @author arungupta
  */
 @XmlRootElement
 public class Person {
-    private String name;
+    private String name ;
 
     public Person() {
     }
 
     public Person(String name) {
-        this.name = name;
+        this.name = name ;
     }
     
     public String getName() {
-        return name;
+        return name ;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name = name ;
     }
 
     @Override
     public String toString() {
-        return name;
+        return name ;
     }
 }
