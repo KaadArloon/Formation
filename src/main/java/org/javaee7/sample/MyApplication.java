24891974e94f505c0c1444c0a2b62f3a4fd0fe60
Jean-Philippe DIDIER <jphdidier@gmail.com>
2016-04-19 15:12:18 +0200

    j'aime les espaces

--- src/main/java/org/javaee7/sample/MyApplication.java
+++ src/main/java/org/javaee7/sample/MyApplication.java
@@ -1,7 +1,7 @@
-package org.javaee7.sample;
+package org.javaee7.sample ;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath ;
+import javax.ws.rs.core.Application ;
 
 @ApplicationPath("/resources")
 public class MyApplication extends Application {
